43cee10f980e59e9baf3cebc05ac1b976e2d1da7
gustavopuche <gustavo.puche@gmail.com>
2022-05-17 15:05:26 +0200

    Full implemented not compiling

--- basic-login/basic-login.cpp
+++ basic-login/basic-login.cpp
@@ -87,9 +87,9 @@ public:
   }
 private:
   void GetSignal();
-  void AnswareLogin();
-  void AnswareCredentials();
-  void AnswareLogout();
+  void AnswareLogin(Message& m);
+  void AnswareCredentials(Message& m);
+  void AnswareLogout(Message& m);
 };
 
 void Signaler::GetSignal() {
@@ -99,8 +99,55 @@ void Signaler::GetSignal() {
     scoped_lock lck{m_inbox};
     Message m = inbox.front();
     cout << "Signal read id: " << m.id << " type: " << m.type << endl;
+
+    // WARNING: inside a lock we are getting another lock.
+    switch(m.type)
+    {
+    case MessageType::login:
+      AnswareLogin(m);
+      break;
+    case MessageType::credentials:
+      AnswareCredentials(m);
+      break;
+    case MessageType::logout:
+      AnswareLogout(m);
+      break;
+    }
     inbox.pop();
   }
+}
+
+void Signaler::AnswareLogin(Message &m) {
+  while (outbox.id != -1)
+  {
+    cout << "Signaler is waiting outbox is read in AnswareLogin"
+    this_thread::sleep_for(milliseconds{20});
+  }
+  scoped_lock lck{m_outbox};
+  outbox.id = m.id;
+  outbox.type = MessageType::login_rec;
+}
+
+void Signaler::AnswareCredentials(Message &m) {
+  while (outbox.id != -1)
+  {
+    cout << "Signaler is waiting outbox is read in AnswareCredentials"
+    this_thread::sleep_for(milliseconds{20});
+  }
+  scoped_lock lck{m_outbox};
+  outbox.id = m.id;
+  outbox.type = MessageType::credentials_rec;
+}
+
+void Signaler::AnswareLogout(Message &m) {
+  while (outbox.id != -1)
+  {
+    cout << "Signaler is waiting outbox is read in AnswareLogout"
+    this_thread::sleep_for(milliseconds{20});
+  }
+  scoped_lock lck{m_outbox};
+  outbox.id = m.id;
+  outbox.type = MessageType::logout_rec;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -116,6 +163,11 @@ public:
   void operator()(){
     cout << "Starting User id: " << id << " name: " << name << endl;
     SendLogin();
+    Get();
+    SendCredentials();
+    Get();
+    SendLogout();
+    Get();
   }
 private:
   int    id;                    // Id of the user.
@@ -125,7 +177,8 @@ private:
   void SendCredentials();
   void SendLogout();
 
-  bool GetAnsware(MessageType type);
+  void Get();
+  bool GetAnsware();
 };
 
 void User::SendLogin() {
@@ -135,6 +188,44 @@ void User::SendLogin() {
   scoped_lock lck{m_inbox}; // acquire inbox mutex.
   cout << "User id: " << id << " name: " << name << " sends login." << endl;
   inbox.push(m);
+}
+
+void User::SendCredentials() {
+  Message m;
+  m.id = id;
+  m.type = MessageType::credentials;
+  m.params.push_back(Param{"user",name});
+  m.params.push_back(Param{"password",name+"_passwd"});
+  scoped_lock lck{m_inbox}; // acquire inbox mutex.
+  cout << "User id: " << id << " name: " << name << " sends credentials." << endl;
+  inbox.push(m);
+}
+
+void User::SendLogout() {
+  Message m;
+  m.id = id;
+  m.type = MessageType::logout;
+  scoped_lock lck{m_inbox}; // acquire inbox mutex.
+  cout << "User id: " << id << " name: " << name << " sends logout." << endl;
+  inbox.push(m);
+}
+
+bool User::GetAnsware() {
+  scoped_lock lck{m_inbox}
+  if (inbox.id == id)
+  {
+    inbox.id = -1;
+    return true;
+  }
+
+  return false;
+}
+
+void User::Get() {
+  while(!GetAnsware())
+  {
+    this_thread::sleep_for(milliseconds{20});
+  }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
